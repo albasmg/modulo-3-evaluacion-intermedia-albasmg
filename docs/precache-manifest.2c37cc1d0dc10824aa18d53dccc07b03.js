e9be102518b50e8808fce3451c9be1bf68a67618
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-07-28 09:02:14 +0200

    initial commit

---
+++ docs/precache-manifest.2c37cc1d0dc10824aa18d53dccc07b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492a57d104d659b66d74ac3c8b69a64d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "108921b79e80fba0b0ea",
+    "url": "./static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5ab32bf72367dc46f40a",
+    "url": "./static/js/2.e5d8d705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e5d8d705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108921b79e80fba0b0ea",
+    "url": "./static/js/main.16258ea2.chunk.js"
+  },
+  {
+    "revision": "093fc2926ed5c52e2c80",
+    "url": "./static/js/runtime-main.e37a2cb8.js"
+  }
+]);
